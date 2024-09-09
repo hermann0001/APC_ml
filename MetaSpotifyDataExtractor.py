ecb3229ae365c7a2e93f81c558a2d010899ca39d
hermann0001 <her.tamilia@stud.uniroma3.it>
2024-09-09 20:01:55 +0200

    working on the challenge set

---
+++ MetaSpotifyDataExtractor.py
@@ -0,0 +1,50 @@
+#little scripts for meta dati of spotify tracks
+import requests # type: ignore
+
+def get_access_token(client_id, client_secret):
+    url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    body = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    response = requests.post(url, headers=headers, data=body)
+    
+    if response.status_code == 200:
+        return response.json().get('access_token')
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+# Utilizzo:
+client_id = '***'
+client_secret = '***'
+token = get_access_token(client_id, client_secret)
+
+if token:
+    print(f"Access Token: {token}")
+
+def get_tracks(access_token, track):
+    url = 'https://api.spotify.com/v1/audio-features/' + track
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
+
+# Utilizzo:
+track_id = '11dFghVXANMlKmJXsNCbNl'
+tracks = get_tracks(token, track_id)
+
+if tracks:
+    print(tracks)
---
+++ read_testdata.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import json
+import os
+
+# data folder
+META_SRC_FOLDER = "formatted/dataset/"
+SRC_FOLDER = "challenge_set/"
+TARGET_FOLDER = "formatted/challenge_set/"
+# files
+TEST_FILE = "challenge_set.json"
+ARTISTS_FILE = "artists.csv"
+TRACKS_FILE = "tracks.csv"
+PLAYLISTS_TRACKS_FILE = "playlists_tracks.csv"
+
+if __name__ == '__main__':
+    artists = pd.read_csv(os.path.join(META_SRC_FOLDER, ARTISTS_FILE))
+    tracks = pd.read_csv(os.path.join(META_SRC_FOLDER, TRACKS_FILE))
+
+    # Create mappings for quick lookup
+    # Ensure index is unique
+    trackmap = tracks[['track_id', 'track_uri']].drop_duplicates().set_index('track_uri')
+    artistmap = artists[['artist_id', 'artist_uri']].drop_duplicates().set_index('artist_uri')
+
+    # Verify that the indices are unique
+    assert trackmap.index.is_unique, "trackmap index is not unique"
+    assert artistmap.index.is_unique, "artistmap index is not unique"
+
+    with open(os.path.join(SRC_FOLDER, TEST_FILE), 'r') as f:
+        challenge_set_json = json.load(f)
+    
+    playlists_challenge = challenge_set_json['playlists']
+
+    # convert the playlists to DataFrame
+    playlists_challenge_df = pd.json_normalize(playlists_challenge)
+    playlists_df = playlists_challenge_df[['pid', 'name', 'num_tracks', 'num_samples', 'num_holdouts']]
+    playlists_df.columns = ['playlist_id', 'name', 'num_tracks', 'num_samples', 'num_holdouts']
+
+    # Explode tracks field
+    tracks_df = playlists_challenge_df[['pid', 'tracks']].explode('tracks').reset_index(drop=True)
+    tracks_df = pd.json_normalize(tracks_df['tracks'])
+
+    print(playlists_challenge_df.head())
+
+
+    # map URIs to IDs
+    tracks_df['artist_id'] = tracks_df['artist_uri'].map(artistmap['artist_id'])
+    tracks_df['track_id'] = tracks_df['track_uri'].map(trackmap['track_id'])
+
+    # Create playlists_track DataFrame
+    playlists_tracks_df = tracks_df[['pid', 'track_id', 'artist_id', 'pos']].rename(columns={'pid': 'playlist_id'}).reset_index(drop=True)
+
+    # Save csv
+    playlists_df.to_csv(os.path.join(TARGET_FOLDER, PLAYLISTS_FILE), index=False)
+    playlists_tracks_df.to_csv(os.path.join(TARGET_FOLDER, PLAYLISTS_TRACKS_FILE), index=False)
