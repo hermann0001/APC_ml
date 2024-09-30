ca0c6b664f8f5d689f83c0921e6f0e093cc38b48
hermann0001 <her.tamilia@stud.uniroma3.it>
2024-09-30 22:09:44 +0200

    logging in clustering

--- clustering.py
+++ clustering.py
@@ -5,6 +5,7 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
 from gensim.models import Word2Vec
+import logging
 
 MDL_FOLDER = 'models/'
 SRC_FOLDER = 'formatted/dataset/'
@@ -12,8 +13,12 @@ SRC_FOLDER = 'formatted/dataset/'
 model = Word2Vec.load(MDL_FOLDER + 'w2v/w2v-trained-model.model')
 
 embedding_matrix = model.wv[model.wv.index_to_key]
+logging.info(f'Embedding matrix shape: {embedding_matrix.shape}')
+
 
 def spherical_kmeans(X, n_clusters, max_iter=300):
+    logging.info(f'Starting Spherical K-Means with {n_clusters} clusters.')
+    
     # Normalize the data
     X_normalized = X / np.linalg.norm(X, axis=1, keepdims=True)
     
@@ -21,9 +26,12 @@ def spherical_kmeans(X, n_clusters, max_iter=300):
     kmeans = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=max_iter)
     kmeans.fit(X_normalized)
     
+    logging.info(f'Finished Spherical K-Means with {n_clusters} clusters.')
     return kmeans.labels_
 
 def locate_optimal_elbow(x, y):
+    logging.info('Calculating optimal elbow point.')
+    
     # Connect the first and last point of the curve with a straight line
     line_start = (x[0], y[0])
     line_end = (x[-1], y[-1])
@@ -31,7 +39,6 @@ def locate_optimal_elbow(x, y):
     # Calculate distances from points to the line
     distances = []
     for i in range(len(y)):
-        # Calculate distance from point (x[i], y[i]) to the line
         numerator = abs((line_end[1] - line_start[1]) * x[i] - (line_end[0] - line_start[0]) * y[i] + 
                         line_end[0] * line_start[1] - line_end[1] * line_start[0])
         denominator = np.sqrt((line_end[1] - line_start[1]) ** 2 + (line_end[0] - line_start[0]) ** 2)
@@ -39,11 +46,13 @@ def locate_optimal_elbow(x, y):
 
     # The index of the maximum distance corresponds to the elbow point
     elbow_index = np.argmax(distances)
+    logging.info(f'Optimal elbow point found at index: {elbow_index}')
     return elbow_index
 
 
 wcss = []  # List to hold Within-Cluster Sum of Squares (WCSS)
 for n_clusters in range(10, 501, 10):  # Iterate over number of clusters
+    logging.info(f'Calculating WCSS for {n_clusters} clusters.')
     labels = spherical_kmeans(embedding_matrix, n_clusters)
     # Calculate WCSS
     wcss.append(sum((np.linalg.norm(embedding_matrix[labels == i] - np.mean(embedding_matrix[labels == i], axis=0)))**2 for i in range(n_clusters)))
@@ -61,6 +70,7 @@ songs_cluster['cluster'] = skm_opt_labels
 songs_cluster['cluster'] = songs_cluster['cluster'].fillna(-1).astype(int).astype('category')
 
 # Visualization using t-SNE
+logging.info('Performing t-SNE visualization...')
 embedding_tsne = TSNE(n_components=2, metric='cosine', random_state=123).fit_transform(embedding_matrix)
 
 # Prepare DataFrame for plotting
@@ -75,4 +85,4 @@ plt.xlabel('t-SNE Component 1')
 plt.ylabel('t-SNE Component 2')
 plt.savefig('figures/clustering.png')
 
-print(f"The optimal number of clusters (elbow point) is: {k_opt + 10}")  # Adjust index since we started from 10
+logging.info(f"The optimal number of clusters (elbow point) is: {k_opt + 10}")  # Adjust index since we started from 10
